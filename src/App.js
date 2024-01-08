ff76fc55edb865113a4fa392aa912a6ca993f5fd
arex123 <ad47kumar@gmail.com>
2024-01-09 00:18:21 +0530

    navbar section done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import './App.css';
+import Body from './Components/Body/Body';
+import Header from './Components/Header/Header';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+
+      <Navbar/>
+      <Header/>
+      <Body/>
+      
+{/* 
+Navbar
+header
+body 
+    overview
+    transactionList
+
+
+*/}
+
+
     </div>
   );
 }
